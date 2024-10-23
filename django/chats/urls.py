9738edd856f7da37c1118feaf582923642c559ea
harune-pg <h.makino.22.6.533@4nonome.com>
2024-10-23 23:18:36 +0900

    add Give/Cancel GoodAPIView

--- django/chats/urls.py
+++ django/chats/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("chat/<uuid:chat_id>/message/polling/", views.ChatMessagePollingListAPIView.as_view()),
     path("chat/latest_message/", views.ChatLatestMessageAPIView.as_view()),
     path("message/create/", views.MessageCreateAPIView.as_view()),
+    path("message/<uuid:message_id>/good/", views.GiveGoodAPIView.as_view()),
+    path("message/<uuid:message_id>/ungood/", views.CancelGoodAPIView.as_view()),
 ]
--- django/chats/views.py
+++ django/chats/views.py
@@ -3,8 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 
-from .models import Chat
-from .serializers import MessageSerialiser, MessagePollingSerializer
+from .models import Chat, Message
+from .serializers import MessageSerialiser, MessagePollingSerializer, GoodSerializer
 
 
 class ChatMessagePollingListAPIView(APIView):
@@ -37,3 +37,19 @@ class MessageCreateAPIView(APIView):
         headers = self.get_success_headers(serializer.data)
         # TODO: 非同期でDifyのAPIを呼び出す
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class GiveGoodAPIView(APIView):
+    def post(self, request, message_id):
+        serializer = GoodSerializer(data={"message": message_id, "player": request.user.id})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"success": "グッドをつけました。"}, status=status.HTTP_200_OK)
+    
+
+class CancelGoodAPIView(APIView):
+    def post(self, request, message_id):
+        message = get_object_or_404(Message, id=message_id)
+        good = message.good_set.get(player=request.user)
+        good.delete()
+        return Response({"success": "グッドを取り消しました。"}, status=status.HTTP_200_OK)
