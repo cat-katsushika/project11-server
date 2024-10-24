bf1adf466daabf29e4d248102af1e52aa43c3cb5
harune-pg <h.makino.22.6.533@4nonome.com>
2024-10-24 15:52:42 +0900

    add migrations

--- django/trials/admin.py
+++ django/trials/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Player, Trial
+from .models import Player, Trial, GameState
 
 admin.site.register(Trial)
 admin.site.register(Player)
+admin.site.register(GameState)
